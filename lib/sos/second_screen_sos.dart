0520a9ecd22a5747bb9092cf6477008b6e74e20c
iamndnt <71444925+iamndnt@users.noreply.github.com>
2023-05-17 00:06:04 +0700

    update sos

--- lib/sos/second_screen_sos.dart
+++ lib/sos/second_screen_sos.dart
@@ -1,7 +1,16 @@
 import 'dart:async';
 
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../db/db_services.dart';
+import '../model/contactsm.dart';
 
 class SecondScreenSOS extends StatefulWidget {
   const SecondScreenSOS({Key? key}) : super(key: key);
@@ -13,19 +22,119 @@ class SecondScreenSOS extends StatefulWidget {
 class _SecondScreenSOSState extends State<SecondScreenSOS> {
 
   int seconds=10;
+  int check=0;
   late Timer timer;
+  Position? _curentPosition;
+  String? _curentAddress;
+
   @override
   void initState() {
     // TODO: implement initState
     startTimer();
+    _getPermission();
+    _getCurrentLocation();
     super.initState();
   }
+  _getPermission() async => await [Permission.sms].request();
+  _getCurrentLocation() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+        forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _curentPosition = position;
+        print(_curentPosition!.latitude);
+        _getAddressFromLatLon();
+      });
+    }).catchError((e) {
+    });
+  }
 
+  _sendSms(String phoneNumber, String message, {int? simSlot}) async {
+    SmsStatus result = await BackgroundSms.sendMessage(
+        phoneNumber: phoneNumber, message: message, simSlot: 1);
+    if (result == SmsStatus.sent) {
+      print("Sent");
+      Fluttertoast.showToast(msg: "send");
+    } else {
+      Fluttertoast.showToast(msg: "failed");
+    }
+  }
+  _callNumber(String number) async {
+    await FlutterPhoneDirectCaller.callNumber(number);
+  }
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  getAndSendSms() async {
+    List<TContact> contactList = await DatabaseHelper().getContactList();
+
+    String messageBody =
+        "https://maps.google.com/?daddr=${_curentPosition!.latitude},${_curentPosition!.longitude}";
+    if (await _isPermissionGranted()) {
+      contactList.forEach((element) {
+        _sendSms("${element.number}", "I am in trouble $messageBody");
+      });
+      contactList.forEach((element) {
+        _callNumber(element.number);
+
+      });
+    } else {
+      Fluttertoast.showToast(msg: "something wrong");
+    }
+  }
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+  _getAddressFromLatLon() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _curentPosition!.latitude, _curentPosition!.longitude);
+
+      Placemark place = placemarks[0];
+      setState(() {
+        _curentAddress =
+        "${place.locality},${place.postalCode},${place.street},";
+      });
+    } catch (e) {
+    }
+  }
   void startTimer(){
     timer=Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         if(seconds!=0)
           seconds--;
+        else{
+          if(check==0)
+            getAndSendSms();
+          check=1;
+        }
+
       });
     });
   }
